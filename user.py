4c90e323dc466f1e7ac5f8fc7e347cc2ff04d15f
Liam Glanfield <liam@glanfield.co.uk>
2018-02-15 14:27:21 +0000

    Create user.py

---
+++ user.py
@@ -0,0 +1,17 @@
+import subprocess
+
+class newuser():
+
+    def encrypt_pass(self,password):
+        c = subprocess.Popen(['mkpasswd', '-m', 'sha-512', password], stdout=subprocess.PIPE)
+        out, err = mkpass_c.communicate()
+        exit_c = c.wait()
+
+        return out.rstrip()
+
+    def check_user(self,username):
+        c = subprocess.Popen(['grep','-c','^' + username + ':','/etc/passwd'], stdout=subprocess.PIPE)
+        out, err = c.communicate()
+        exit_c = c.wait()
+
+        return out.rstrip()
